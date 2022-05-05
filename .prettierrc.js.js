b1373b89448ba844edb4cc2a9307c44ab898f6e9
wuhaomin <786438024@qq.com>
2022-05-05 14:57:17 +0800

    ci: 增加vue文件保存默认格式化

---
+++ .prettierrc.js.js
@@ -0,0 +1,14 @@
+module.exports = {
+	printWidth: 120,
+	tabWidth: 2,
+	tabs: false,
+	semi: true,
+	singleQuote: true,
+	quoteProps: 'as-needed',
+	tailingComma: 'all',
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	arrowParens: 'always',
+	htmlWhitespaceSensitivity: 'ignore',
+	useTabs: true,
+};
